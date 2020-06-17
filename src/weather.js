338a8badd7cfd7daefdfd2165da5e6032dafd6e5
AhsanulHaq777 <66363908+AhsanulHaq777@users.noreply.github.com>
2020-06-17 09:29:29 +0500

    Update props in weather.js File

--- src/weather.js
+++ src/weather.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function TodayWeather (props){
     return(
-        <div>Today weather is {props.checkweathr} </div>
+        <div>Today weather is {props.checkweather} </div>
     );
 }
 export default TodayWeather;
